7b55eb7aa5164a2c4e12c58658bd9e753016a838
jjbillings <jackjbill@gmail.com>
2019-07-29 18:52:55 -0400

    Remove users_controller
    The `users_controller` and associated views, routes, etc. are not needed when using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :savings_goals
 
-  resources :users do |users|
-    resources :savings_goals
-  end
-
   get 'welcome/index'
 
   root 'welcome#index'
