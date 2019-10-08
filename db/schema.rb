34d96ed281f0f2eb2f4f2661b4b151524dc9f9d4
jjbillings <jackjbill@gmail.com>
2019-10-08 19:28:16 -0400

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_19_002024) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.integer "user_id"
     t.integer "account_type"
