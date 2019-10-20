a93f21d4e6b01907ccf2438ef82048f05ff774e9
jjbillings <jackjbill@gmail.com>
2019-10-20 10:03:32 -0400

    Build out transactions views
    - Add account and expense selectors to the transaction form
    - Add links to transaction's expense and account on the index/show pages

--- spec/views/transactions/edit.html.erb_spec.rb
+++ spec/views/transactions/edit.html.erb_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "transactions/edit", type: :view do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { create(:user) }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
   let!(:transaction) { create(:transaction) }
   before(:each) { assign(:transaction, transaction) }
 
--- spec/views/transactions/show.html.erb_spec.rb
+++ spec/views/transactions/show.html.erb_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "transactions/show", type: :view do
-  let!(:transaction) { create(:transaction) }
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { create(:user) }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  let(:expense) { create(:expense, user: user) }
+  let(:account) { create(:account, user: user) }
+  let!(:transaction) { create(:transaction, { account: account, expense: expense }) }
+
   before(:each) { assign(:transaction, transaction) }
 
   it "renders attributes in <p>" do
