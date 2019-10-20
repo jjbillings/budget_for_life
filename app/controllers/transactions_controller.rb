f50150712c51c67bb63cf218f5758027f22b243d
jjbillings <jackjbill@gmail.com>
2019-10-20 16:00:37 -0400

    Retrieve user's transactions
    Aggregate all of a user's transactions through their accounts

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,7 +7,7 @@ class TransactionsController < ApplicationController
   # GET /transactions
   # GET /transactions.json
   def index
-    @transactions = Transaction.all
+    @transactions = current_user.transactions
   end
 
   # GET /transactions/1
